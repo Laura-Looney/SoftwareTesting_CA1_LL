8dba3a717f4247906efb700703e83df25de3ab5a
t00223553 <laura.looney@students.ittralee.ie>
2023-03-27 10:48:05 +0100

    ihui

---
+++ Test/LoanTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanTest
+    {
+        @Test
+        public void test1(){
+            Loan loan =  new Loan(500,1);
+        assertEquals(43.96,loan.getMonthlyPayment());
+        }
+
+    }
